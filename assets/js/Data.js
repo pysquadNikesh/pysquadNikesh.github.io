0df8238bb231f35e984305fd4b268f86039c47d9
pysquadNikesh <159936264+pysquadNikesh@users.noreply.github.com>
2025-01-16 16:51:51 +0530

    Add files via upload

---
+++ assets/js/Data.js
@@ -0,0 +1,245 @@
+export const UserData = [
+  { image: "assets/images/PYS_Icon.svg" },
+  { name: "Nikesh Chaudhary" },
+  { social: "22 Nov. 2000" },
+  { email: "nikesh@pysquad.com" },
+  { phone: "+91 9825241294" },
+  { location: "A 605, Shilp Aaron,Sindhu Bhavan Road, Ahmedabad-380054" },
+  { designation: "Odoo Expert, <br>Techno Functional" },
+  { website: "https://pysquad.com" },
+];
+
+export const profile = [
+  {
+    title: "Software Developer",
+    summary:
+      "With over seven years of hands-on experience in Odoo, I bring a deep understanding of the platform, spanning versions 10.0 to 18.0, and am certified in Odoo 17.0. My expertise covers a wide range of modules, including CRM, Sales, Accounting, Inventory, MRP, Project Management, HRMS, and E-commerce. I specialize in developing REST APIs, handling database migrations, integrating third-party applications, setting up SAAS solutions, and customizing Odoo for unique business needs. <br> <br>I take pride in delivering tailored, results-driven solutions that align with my clients' goals. Always eager to grow and innovate, I’m expanding my knowledge in React JS to stay ahead in the ever-evolving tech landscape.",
+  },
+];
+
+export const services = [
+  {
+    image: "assets/images/Implementation.svg",
+    title: "Implementation",
+    description:
+      "Seamlessly deploy Odoo tailored to your business needs, ensuring a smooth transition and optimal performance.",
+  },
+  {
+    image: "assets/images/Cuntomization.svg",
+    title: "Customisation / Development",
+    description:
+      "Enhance Odoo functionalities with tailored modules and workflows to meet unique business requirements.",
+  },
+  {
+    image: "assets/images/Migration.svg",
+    title: "Migration / Integration",
+    description:
+      "Effortlessly migrate data to the latest Odoo version and integrate it with third-party applications for seamless operations.",
+  },
+  {
+    image: "assets/images/Consultation.svg",
+    title: "Consultation",
+    description:
+      "Expert guidance to strategize, plan, and maximize the efficiency of Odoo for your business success.",
+  },
+];
+
+export const education = [
+  { degree: "MCA", institute: "Gujarat Vidyapith", year: "2015 - 2018" },
+
+  {
+    degree: "BCA",
+    institute: "HNGU",
+    year: "2012 - 2015",
+  },
+];
+
+export const experience = [
+  {
+    designation: "Sr. Odoo Developer",
+    company: "Freelancer",
+    year: "2020 - Present",
+  },
+
+  {
+    designation: "Odoo Developer",
+    company: "Techulta Solutions Pvt Ltd",
+    year: "2019 - 2020",
+  },
+
+  {
+    designation: "Odoo Developer",
+    company: "Serpent Consulting Services Pvt Ltd",
+    year: "2018 - 2019",
+  },
+];
+
+export const skills = [
+  { name: "Python", progress: "75" },
+  { name: "Odoo", progress: "90" },
+  { name: "Migration", progress: "90" },
+  { name: "Database Migration", progress: "80" },
+  { name: "Payment Gatway", progress: "70" },
+  { name: "Third Party Integration", progress: "85" },
+  { name: "PostgreSQL", progress: "70" },
+  { name: "MySQL", progress: "70" },
+  { name: "Git", progress: "90" },
+  { name: "Tryton", progress: "60" },
+  { name: "Docker", progress: "70" },
+  { name: "Odoo.sh", progress: "80" },
+  { name: "AWS / Digital Ocean / Google Cloud", progress: "70" },
+  { name: "Rest API", progress: "90" },
+  { name: "Fast API", progress: "65" },
+  { name: "Web Scraping", progress: "50" },
+  { name: "JavaScript", progress: "60" },
+  { name: "CSS", progress: "60" },
+];
+
+export const projects = [
+  {
+    name: "Edumarket",
+    image: "assets/images/projects/Edumarket.png",
+    page: "assets/images/projects/Edumarket.png",
+    description: "odoo modules worked: sales, e-commerce, rest API.",
+    alt: "Edumarket",
+    technologies: "Python, Odoo-11.0, JSON",
+    category: "", // Added category
+  },
+
+  {
+    name: "Narayana Educational Trust",
+    image: "assets/images/projects/Narayana.png",
+    page: "assets/images/projects/Narayana.png",
+    description:
+      "it contains around 1400+ schools and colleges under him. maintain all student details. uograde them school to college also teachers payroll and digital payments.",
+    alt: "Narayana Educational Trust",
+    technologies: "Python, Odoo-11.0",
+    category: "", // Added category
+  },
+
+  {
+    name: "Gujarat council of Science City",
+    image: "assets/images/projects/Science_city.png",
+    page: "assets/images/projects/Science_city.png",
+    description:
+      "Gujarat Science City is a bold initiative of the Government of Gujarat to realize this priority. The Government is creating a sprawling center at Ahmedabad which aims to provide a perfect blend of education and entertainment. It will showcase contemporary and imaginative exhibits, minds on experiences, working models, virtual reality, activity corners, labs and live demonstrations to provide an understanding of science and technology to the common man.",
+    alt: "Gujarat council of Science City",
+    technologies: "Python, Odoo-13.0, Js",
+    category: "", // Added category
+  },
+  {
+    name: "Bethliving",
+    image: "assets/images/projects/Ebethliving.png",
+    page: "assets/images/projects/Ebethliving.png",
+    description:
+      "Beth Living introduces modular home décor furniture in Steel and Stainless Steel. Beth Living products transforms your home into a fresh, modern looking one, giving your home a contemprary decor. Because your family is precious, and your loved ones deserve the best. Beth's modern alternative home decor products are designed and developed to protect your family with its unmatched features.",
+    alt: "Bethliving",
+    technologies: "Python, Odoo-14.0, JavaScript, HTML, CSS",
+    category: "", // Added category
+  },
+
+  {
+    name: "Ayana Power",
+    image: "assets/images/projects/AyanaPower.png",
+    page: "assets/images/projects/AyanaPower.png",
+    description:
+      "AYANAPOWER is an utility-scale renewable energy platform dedicated to expanding India’s renewable energy footprint, helping it achieve its 500 GW renewable energy target by 2030. Backed by some of the most significant investment funds - National Investment & Infrastructure Fund, British International Investment, and Green Growth Equity Fund, we support a low-carbon future while helping create jobs in the power sector. We are powered by a team of experts, with vast experience in the industry. Our wide range of renewable energy resources such as solar, wind, round-the-clock, and green hydrogen grants us a solid industry position. We strive towards ensuring a greener and brighter future through innovation and the empowerment of local communities.",
+    alt: "AYANAPOWER",
+    technologies: "Python, Odoo-14.0",
+    category: "", // Added category
+  },
+  {
+    name: "Golden Falcon Aviation",
+    image: "assets/images/projects/GoldenFalcon.png",
+    page: "assets/images/projects/GoldenFalcon.png",
+    description:
+      "Turbify, an Infinite company, provides tools and resources businesses need to succeed. It starts with a custom domain name and we have over 400 top level domains including .com, .info, .shop and more to help your business get the perfect web address. Next is your website, and we have a range of products from the easy self-service option with our Websites builder, hosting to allow you to build more custom websites using WordPress or other tools, and Stores for those building ecommerce businesses. We can even design, build and manage your website for you with Website Design Services.",
+    alt: "Golden Falcon Aviation",
+    technologies: "Python, Odoo-14.0, JavaScript, Mysql, OCR",
+    category: "", // Added category
+  },
+
+  {
+    name: "Anker Innovation (E-commerce, Kuwait)",
+    image: "assets/images/projects/Anker.png",
+    page: "assets/images/projects/Anker.png",
+    description:
+      " Astore is Kuwait’s leading mobile accessories & smart gadgets destination. With exactly 999 products on display, we select only the very best of what the industry has to offer. Not only that, ASTORE trademark also represents other affiliated trade marks. Fixaid By Astore; that is Kuwait's favorite on-the-go maintenance center. We are the exclusive distributor for the highest quality grade brands mainly in Kuwait and the Gulf area.",
+    alt: "Anker Innovation",
+    technologies: "Python, Odoo-14.0, JavaScript, Mysql",
+    category: "", // Added category
+  },
+  {
+    name: "Gujarat FibreGrid (Govt. Project)",
+    image: "assets/images/projects/GFGNL.png",
+    page: "assets/images/projects/GFGNL.png",
+    description:
+      "Overseeing Gujarat's FiberGrid network, integrating billing and HRMS for efficiency. Ensures seamless connectivity, automates billing, HR functions, and network monitoring.",
+    alt: "Gujarat FibreGrid",
+    technologies: "Python, Odoo-13.0, XML",
+    category: "", // Added category
+  },
+
+  {
+    name: "Bonyan",
+    image: "assets/images/projects/Bonyan.png",
+    page: "assets/images/projects/Bonyan.png",
+    description:
+      "Bonyan is a leading NGO focused on rebuilding and revitalizing war-impacted communities and aiding refugees within the U.S. and abroad. As a trusted name in philanthropy, we’ve dedicated ourselves to transforming lives, advocating for quality education, and fostering community growth. Your donations drive our mission, making every effort possible.",
+    alt: "Bonyan",
+    technologies: "Python, Odoo-17.0, Javascript",
+    category: "", // Added category
+  },
+
+  {
+    name: "Codot Logistics",
+    image: "assets/images/projects/Codot.png",
+    page: "assets/images/projects/Codot.png",
+    description:
+      "I collaborated with CODOT Logistics to implement and customize a leading ERP system, enhancing their operational efficiency and transparency. The project involved automating office operations, improving shipment tracking with real-time updates, and resolving logistical challenges through tailored IT solutions. This work enabled CODOT to adapt to market changes, boost customer satisfaction, and achieve significant business growth.",
+    alt: "Codot Logistics",
+    technologies: "Python, Odoo-14.0, Javascript",
+    category: "",
+  },
+
+  {
+    name: "vapi.co Connector",
+    image: "assets/images/projects/Vapi.png",
+    page: "assets/images/projects/Vapi.png",
+    description:
+      "Developed a custom Odoo module to integrate seamlessly with Vapi.co, tailored specifically for the pharmaceutical industry. The module enables effortless synchronization of data between Vapi.co and Odoo, ensuring accurate and efficient management of critical business operations. Key functionalities include fetching and synchronizing user details, products, orders, and other essential data, while incorporating customizations in Odoo to align with the unique requirements of the pharma sector. This solution streamlined data flow, enhanced operational efficiency, and provided a robust foundation for managing pharmaceutical operations effectively.",
+    alt: "vapi.co Connector",
+    technologies: "Python, Odoo-18.0, API",
+    category: "",
+  },
+
+  {
+    name: "Quickbook Connector",
+    image: "assets/images/projects/QuickBook.png",
+    page: "assets/images/projects/QuickBook.png",
+    description:
+      "Developed a QuickBooks connector for Odoo, enabling seamless synchronization of financial and operational data between the two platforms. This integration automates the transfer of key data, such as invoices, payments, customers, vendors, and accounts, ensuring consistency and accuracy across systems. The connector facilitates real-time updates and reduces manual data entry, enhancing efficiency and minimizing errors. By bridging Odoo's robust ERP capabilities with QuickBooks' comprehensive accounting features, this solution provides businesses with a streamlined workflow for financial management and operational excellence.",
+    alt: "Quickbook Connector",
+    technologies: "Python, Odoo-17.0, API",
+    category: "",
+  },
+
+  {
+    name: "OCR",
+    image: "assets/images/projects/Ocr.png",
+    page: "assets/images/projects/Ocr.png",
+    description:
+      "Developed an OCR functionality using various models to automatically extract information from emails received through the Incoming Mail Server. This feature creates records in the Sales Order and Leads modules in Odoo, capturing product details and other relevant information to streamline data entry and improve workflow efficiency.",
+    alt: "OCR",
+    technologies:
+      "Python, Odoo-17.0, Python Libraries (poplib, tabula, camelot, imaplib)",
+    category: "",
+  },
+];
+
+export const certificates = [
+  {
+    name: "Odoo 17 Functional Certificate",
+    image: "assets/images/odoo_17_certification.png",
+  },
+];
